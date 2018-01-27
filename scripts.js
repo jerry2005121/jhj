31a0e75728b758707fdca731f713853f8fa82d9d
jerry <jerry1995121@icloud.com>
2018-01-27 14:32:31 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var x = "hello";
+var u = "second message";
+var q = "third message";
+
+
+function myFunction(p1,p2) {
+    return p1 * p2;
+    
+
+}
+
+function myOtherFunction(num1, num2) {
+    alert (num1 * num2);
+    
+}
+
+
+
+
+alert(myFunction(8,9));
